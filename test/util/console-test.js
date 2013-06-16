aca0610b14fecbd2625e02d50a1f8c788fca4f87
nozer0 <c.nozer0@gmail.com>
2013-06-16 16:19:03 +0800

    Remove `define` wrapper from test js files
    Use global `require` function directly on the script

--- test/util/console-test.js
+++ test/util/console-test.js
@@ -1,15 +1,16 @@
-/*global define */
-define(function (require) {
-	'use strict';
-
-	var assert = require('util/assert'), console = require('util/console').constructor();
-	require('util/test').run({
-		testLog	: function() {
-			assert.equal(console.log('hello', 'world'), 'hello world');
-			assert.equal(console.log('%s world', 'hello'), 'hello world');
-			assert.equal(console.log('%d, %d, %d', 1, 2, 3), '1, 2, 3');
-			assert.equal(console.log('%d, %d, %d', 1, 2), '%d, %d, %d 1 2');
-			assert.equal(console.log('%d, %d, %d', 1, 2, 3, 4), '1, 2, 3 4');
-		}
-	});
+/*global require */
+require('util/test').run({
+	name    : 'console',
+	setUp   : function () {
+		this.assert = require('util/assert');
+		this.console = require('util/console').constructor();
+	},
+	testLog : function () {
+		var assert = this.assert, console = this.console;
+		assert.equal(console.log('hello', 'world'), 'hello world');
+		assert.equal(console.log('%s world', 'hello'), 'hello world');
+		assert.equal(console.log('%d, %d, %d', 1, 2, 3), '1, 2, 3');
+		assert.equal(console.log('%d, %d, %d', 1, 2), '%d, %d, %d 1 2');
+		assert.equal(console.log('%d, %d, %d', 1, 2, 3, 4), '1, 2, 3 4');
+	}
 });
--- test/util/uri-test.js
+++ test/util/uri-test.js
@@ -1,50 +1,52 @@
-/*global define */
-define(function (require) {
-	'use strict';
-
-	var assert = require('util/assert'), uri = require('util/uri'), cbase = 'http://localhost/o_p/tests/util/uri.html';
-	require('util/test').run({
-		name          : 'uri',
-		setUp         : function () {
-			uri.config({
-				base  : 'http://www.nozer0.com/foo/bar/some/office',
-				maps  : ['{locale}', 'en-us', /(\w)(?=\.js)/, '$1-debug', 'foo.com', 'foo2.com']
-			});
-		},
-		testLocation  : function () {
-			assert.deepEqual(uri.location('http://nozer0.com:8080/foo/bar/o_p.min.js?v=1.0#debug'), {uri : 'http://nozer0.com:8080/foo/bar/o_p.min.js?v=1.0#debug', protocol : 'http:', host : 'nozer0.com:8080', hostname : 'nozer0.com', port : '8080', pathname : '/foo/bar/o_p.min.js', basename : 'o_p.min', ext : '.js', search : '?v=1.0', hash : '#debug'});
-			assert.deepEqual(uri.location('nozer0.com/foo/bar/o_p.#debug'), {uri : 'http://nozer0.com/foo/bar/o_p.#debug', protocol : 'http:', host : 'nozer0.com', hostname : 'nozer0.com', port : '', pathname : '/foo/bar/o_p.', basename : 'o_p', ext : '.', search : '', hash : '#debug'});
-			assert.deepEqual(uri.location('nozer0.com/foo/bar/o_p/#debug'), {uri : 'http://nozer0.com/foo/bar/o_p/#debug', protocol : 'http:', host : 'nozer0.com', hostname : 'nozer0.com', port : '', pathname : '/foo/bar/o_p/', basename : '', ext : '', search : '', hash : '#debug'});
-			assert.deepEqual(uri.location('http://nozer0.com/?v=1.0'), {uri : 'http://nozer0.com/?v=1.0', protocol : 'http:', host : 'nozer0.com', hostname : 'nozer0.com', port : '', pathname : '/', basename : '', ext : '', search : '?v=1.0', hash : ''});
-		},
-		testNormalize : function () {
-			assert.equal(uri.normalize('http://nozer0.com/a/b/c'), 'http://nozer0.com/a/b/c');
-			assert.equal(uri.normalize('http:///nozer0.com/a//..///./b//.///c//'), 'http://nozer0.com/b/c/');
-			assert.equal(uri.normalize('http://nozer0.com/a//b/c/..///./../../d//../e.js'), 'http://nozer0.com/e.js');
-			assert.equal(uri.normalize('a/../b/../c'), 'c');
-			assert.equal(uri.normalize('a/../../c'), '../c');
-			assert.equal(uri.normalize('//a/b/../../c'), '/c');
-		},
-		testNormal    : function () {
-			assert.equal(uri.resolve('http://foo.com'), 'http://foo2.com');
-			assert.equal(uri.resolve('{locale}/me'), 'http://www.nozer0.com/foo/bar/some/en-us/me');
-			assert.equal(uri.resolve('{locale}/test.min.1.0.js'), 'http://www.nozer0.com/foo/bar/some/en-us/test.min.1.0-debug.js');
-			assert.equal(uri.resolve('js/test.js', cbase), 'http://localhost/o_p/tests/util/js/test-debug.js');
-			assert.equal(uri.resolve('js/test.js', 'util/css'), 'util/js/test-debug.js');
-		},
-		testRelative  : function () {
-			assert.equal(uri.resolve('./me'), 'http://www.nozer0.com/foo/bar/some/me');
-			assert.equal(uri.resolve('./me', 'http://foo.com/'), 'http://foo2.com/me');
-			assert.equal(uri.resolve('../me'), 'http://www.nozer0.com/foo/bar/me');
-			assert.equal(uri.resolve('../../../thank/you/../all'), 'http://www.nozer0.com/thank/all');
-			assert.equal(uri.resolve('../util/js/test.js', cbase), 'http://localhost/o_p/tests/util/js/test-debug.js');
-			assert.equal(uri.resolve('../util/../js/test.js', 'o_p/test/css'), 'o_p/js/test-debug.js');
-		},
-		testAbsolute  : function () {
-			assert.equal(uri.resolve('/me'), 'http://www.nozer0.com/me');
-			assert.equal(uri.resolve('/me', 'http://foo.com/hello/all/'), 'http://foo2.com/me');
-			assert.equal(uri.resolve('/o_p/tests/js/test.js', cbase), 'http://localhost/o_p/tests/js/test-debug.js');
-			assert.equal(uri.resolve('/util/js/test.js', 'o_p/test/css'), '/util/js/test-debug.js');
-		}
-	});
+/*global require */
+require('util/test').run({
+	name          : 'uri',
+	setUp         : function () {
+		this.assert = require('util/assert');
+		this.uri = require('util/uri');
+		this.uri.config({
+			base : 'http://www.nozer0.com/foo/bar/some/office',
+			maps : ['{locale}', 'en-us', /(\w)(?=\.js)/, '$1-debug', 'foo.com', 'foo2.com']
+		});
+	},
+	testLocation  : function () {
+		var assert = this.assert, uri = this.uri;
+		assert.deepEqual(uri.location('http://nozer0.com:8080/foo/bar/o_p.min.js?v=1.0#debug'), {uri : 'http://nozer0.com:8080/foo/bar/o_p.min.js?v=1.0#debug', protocol : 'http:', host : 'nozer0.com:8080', hostname : 'nozer0.com', port : '8080', pathname : '/foo/bar/o_p.min.js', basename : 'o_p.min', ext : '.js', search : '?v=1.0', hash : '#debug'});
+		assert.deepEqual(uri.location('nozer0.com/foo/bar/o_p.#debug'), {uri : 'http://nozer0.com/foo/bar/o_p.#debug', protocol : 'http:', host : 'nozer0.com', hostname : 'nozer0.com', port : '', pathname : '/foo/bar/o_p.', basename : 'o_p', ext : '.', search : '', hash : '#debug'});
+		assert.deepEqual(uri.location('nozer0.com/foo/bar/o_p/#debug'), {uri : 'http://nozer0.com/foo/bar/o_p/#debug', protocol : 'http:', host : 'nozer0.com', hostname : 'nozer0.com', port : '', pathname : '/foo/bar/o_p/', basename : '', ext : '', search : '', hash : '#debug'});
+		assert.deepEqual(uri.location('http://nozer0.com/?v=1.0'), {uri : 'http://nozer0.com/?v=1.0', protocol : 'http:', host : 'nozer0.com', hostname : 'nozer0.com', port : '', pathname : '/', basename : '', ext : '', search : '?v=1.0', hash : ''});
+	},
+	testNormalize : function () {
+		var assert = this.assert, uri = this.uri;
+		assert.equal(uri.normalize('http://nozer0.com/a/b/c'), 'http://nozer0.com/a/b/c');
+		assert.equal(uri.normalize('http:///nozer0.com/a//..///./b//.///c//'), 'http://nozer0.com/b/c/');
+		assert.equal(uri.normalize('http://nozer0.com/a//b/c/..///./../../d//../e.js'), 'http://nozer0.com/e.js');
+		assert.equal(uri.normalize('a/../b/../c'), 'c');
+		assert.equal(uri.normalize('a/../../c'), '../c');
+		assert.equal(uri.normalize('//a/b/../../c'), '/c');
+	},
+	testNormal    : function () {
+		var assert = this.assert, uri = this.uri;
+		assert.equal(uri.resolve('http://foo.com'), 'http://foo2.com');
+		assert.equal(uri.resolve('{locale}/me'), 'http://www.nozer0.com/foo/bar/some/en-us/me');
+		assert.equal(uri.resolve('{locale}/test.min.1.0.js'), 'http://www.nozer0.com/foo/bar/some/en-us/test.min.1.0-debug.js');
+		assert.equal(uri.resolve('js/test.js', 'http://another.domain.com/tests/util/uri.html'), 'http://another.domain.com/tests/util/js/test-debug.js');
+		assert.equal(uri.resolve('js/test.js', 'util/css'), 'util/js/test-debug.js');
+	},
+	testRelative  : function () {
+		var assert = this.assert, uri = this.uri;
+		assert.equal(uri.resolve('./me'), 'http://www.nozer0.com/foo/bar/some/me');
+		assert.equal(uri.resolve('./me', 'http://foo.com/'), 'http://foo2.com/me');
+		assert.equal(uri.resolve('../me'), 'http://www.nozer0.com/foo/bar/me');
+		assert.equal(uri.resolve('../../../thank/you/../all'), 'http://www.nozer0.com/thank/all');
+		assert.equal(uri.resolve('../util/js/test.js', 'http://another.domain.com/tests/util/uri.html'), 'http://another.domain.com/tests/util/js/test-debug.js');
+		assert.equal(uri.resolve('../util/../js/test.js', 'o_p/test/css'), 'o_p/js/test-debug.js');
+	},
+	testAbsolute  : function () {
+		var assert = this.assert, uri = this.uri;
+		assert.equal(uri.resolve('/me'), 'http://www.nozer0.com/me');
+		assert.equal(uri.resolve('/me', 'http://foo.com/hello/all/'), 'http://foo2.com/me');
+		assert.equal(uri.resolve('/o_p/tests/js/test.js', 'http://another.domain.com/tests/util/uri.html'), 'http://another.domain.com/o_p/tests/js/test-debug.js');
+		assert.equal(uri.resolve('/util/js/test.js', 'o_p/test/css'), '/util/js/test-debug.js');
+	}
 });
